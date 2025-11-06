d3c9f21e9d09acdfb62e6ce9828b22901f66ccc2
Emanuele Pavan <emanuele.pav@gmail.com>
2025-11-06 23:57:14 +0100

    implements ChunkedUploader && unit tests

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,11 +1,14 @@
 import { type ReactElement } from 'react';
 
+import ChunkedUploader from './ChunkedUploader';
 import Uploader from './Uploader';
-import { uploadSingle } from './api';
+import { uploadChunk, uploadSingle } from './api';
 
 // Create a minimal page that integrates your upload component. Use the provided demo API to upload files.
 export const App = (): ReactElement => (
     <main className="h-dvh grid grid-flow-col gap-x-8 place-items-center place-content-center">
+        {/* 2 uploader's versions of with similar interfaces */}
+        <ChunkedUploader uploadChunk={uploadChunk} />
         <Uploader uploadSingle={uploadSingle} withFileList />
     </main>
 );
